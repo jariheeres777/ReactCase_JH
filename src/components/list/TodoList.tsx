6b51d03501b1631d448d7139831adbf28a92d25c
jari heeres <jheeres@thinkwisesoftware.com>
2021-10-01 11:16:47 +0200

    start new laptop

--- src/components/list/TodoList.tsx
+++ src/components/list/TodoList.tsx
@@ -1,11 +1,5 @@
 import * as React from 'react';
-import {compose} from 'recompose';
-import {IListActions, IListState, withLists} from '../../state/containers/list.container';
-import {Divider, ListItem, ListItemText, List, Button, TextField, InputLabel,} from '@material-ui/core';
 import StyledTodoList from './styled/StyledTodoList';
-import {IList} from '../../model/interfaces/IList';
-import {v4 as uuid} from 'uuid';
-import {useState} from "react";
 import AddTodoList from './AddTodolist'
 import ShowTodoList from './ShowTodoList';
 
